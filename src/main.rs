91a4f19d1865e5c36e073614d6ffd16836020e71
Hocine <hocine.hammouche@hotmail.com>
2021-06-10 12:45:36 +0200

    Added a method that tells if the board is empty or not

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
 	println!("Hello, world!");
 }
 
+#[derive(PartialEq, Copy, Clone)]
 enum Player { Cross, Circle, None }
 
 struct Board([[Player; 3]; 3]);
@@ -9,5 +10,13 @@ struct Board([[Player; 3]; 3]);
 impl Board {
 	fn new() -> Board {
 		Board([[Player::None; 3]; 3])
+	}
+	fn is_full(&self) -> bool {
+		for row in 0..3 {
+			for col in 0..3 {
+				if self.0[row][col] == Player::None { return false; }
+			}
+		}
+		true
 	}
 }
