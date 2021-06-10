18e609ac4de950dd46f22c015843d199468320e8
Hocine <hocine.hammouche@hotmail.com>
2021-06-10 13:33:13 +0200

    Implementation of the board display
    Implemented the Display trait for the Player enumeration and added a method for the Board object to display itself.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::fmt;
+
 fn main() {
-	println!("Hello, world!");
+	let board = Board::new();
+	board.display();
 }
 
 #[derive(PartialEq, Copy, Clone)]
 enum Player { Cross, Circle, None }
+
+impl fmt::Display for Player {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Player::Cross => write!(f, "X"),
+			Player::Circle => write!(f, "O"),
+			Player::None => write!(f, " "),
+		}
+	}
+}
 
 struct Board([[Player; 3]; 3]);
 
@@ -18,5 +31,12 @@ impl Board {
 			}
 		}
 		true
+	}
+	fn display(&self) {
+		println!("+---+---+---+");
+		for row in self.0.iter() {
+			println!("| {} | {} | {} |", row[0], row[1], row[2]);
+			println!("+---+---+---+");
+		}
 	}
 }
