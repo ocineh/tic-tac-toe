b08aacc552c56d49a3b6b5e81bef8cfff60f46b2
Hocine <hocine.hammouche@hotmail.com>
2021-06-10 14:39:26 +0200

    Added a method that manages players strokes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use std::fmt;
 
 fn main() {
-	let board = Board::new();
+	let mut board = Board::new();
+	board.player_stroke(Player::Circle, 5);
+	board.player_stroke(Player::Cross, 1);
+	board.player_stroke(Player::Circle, 3);
+	board.player_stroke(Player::Cross, 4);
+	board.player_stroke(Player::Circle, 6);
+	board.player_stroke(Player::Cross, 7);
 	board.display();
+	println!("is full: {}", board.is_full());
+	println!("check winner: '{}'", board.check_winner());
 }
 
 #[derive(PartialEq, Copy, Clone)]
@@ -57,5 +65,14 @@ impl Board {
 		else if self.0[0][0] == Player::Circle && self.0[1][1] == Player::Circle && self.0[2][2] == Player::Circle { Player::Circle }
 		else if self.0[2][0] == Player::Circle && self.0[1][1] == Player::Circle && self.0[0][2] == Player::Circle { Player::Circle }
 		else { Player::None }
+	}
+	fn player_stroke(&mut self, player: Player, pos: i8) -> bool {
+		let row: usize = ((pos - 1) / 3) as usize;
+		let col: usize = ((pos - 1) % 3) as usize;
+		if self.0[row][col] == Player::None {
+			self.0[row][col] = player;
+			true
+		}
+		else { false }
 	}
 }
