a503bcd0defe223113598df8860da57282954598
Hocine <hocine.hammouche@hotmail.com>
2021-06-10 22:57:00 +0200

    Added the possibility to make games between two players
    Added a module to manage game, a function that asks the user to choose one of the cells on the board and a function that makes two players play against each other.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod board{
+pub mod board {
 	use std::fmt;
 
 	#[derive(PartialEq, Copy, Clone)]
@@ -55,6 +55,7 @@ pub mod board{
 			else { Player::None }
 		}
 		pub fn player_stroke(&mut self, player: Player, pos: i8) -> bool {
+			if pos < 1 || pos > 9 { return false; }
 			let row: usize = ((pos - 1) / 3) as usize;
 			let col: usize = ((pos - 1) % 3) as usize;
 			if self.0[row][col] == Player::None {
@@ -62,6 +63,54 @@ pub mod board{
 				true
 			}
 			else { false }
+		}
+	}
+}
+
+pub mod game {
+	use super::board::{Board, Player};
+	use std::io::{Write, self};
+
+	fn ask_cell(current_player: Player) -> Result<i8, std::num::ParseIntError> {
+		match current_player {
+			Player::Cross => print!("The player with the cross must choose an empty cell: "),
+			Player::Circle => print!("The player with the circle must choose an empty cell: "),
+			Player::None => panic!("The function must always be called with a current player being either the cross or the circle."),
+		}
+		io::stdout().flush().unwrap();
+
+		let mut cell = String::new();
+		io::stdin()
+			.read_line(&mut cell)
+			.expect("Error reading user input.");
+
+		cell.trim().parse::<i8>()
+	}
+
+	pub fn against_another_player() {
+		let mut board = Board::new();
+		let mut current_player = Player::Cross;
+
+		while !board.is_full() && board.check_winner() == Player::None {
+			print!("{esc}[2J{esc}[0;0H", esc = 27 as char);
+			println!("Current game:");
+			board.display();
+
+			if let Ok(cell) = ask_cell(current_player) {
+				if board.player_stroke(current_player, cell) {
+					current_player = if current_player == Player::Cross { Player::Circle } else { Player::Cross };
+				}
+			}
+		}
+
+		print!("{esc}[2J{esc}[0;0H", esc = 27 as char);
+		println!("Game over");
+		board.display();
+
+		match board.check_winner() {
+			Player::Cross => println!("Victory of the player with the cross."),
+			Player::Circle => println!("Victory of the player with the circle."),
+			Player::None => println!("The game ended in a draw."),
 		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 mod lib;
-use lib::board::{Board, Player};
+use lib::game;
 
 fn main() {
-	let mut board = Board::new();
-	board.player_stroke(Player::Circle, 5);
-	board.player_stroke(Player::Cross, 1);
-	board.player_stroke(Player::Circle, 3);
-	board.player_stroke(Player::Cross, 4);
-	board.player_stroke(Player::Circle, 6);
-	board.player_stroke(Player::Cross, 7);
-	board.display();
-	println!("is full: {}", board.is_full());
-	println!("check winner: '{}'", board.check_winner());
+	game::against_another_player();
 }
