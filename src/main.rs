65c54d21000e0528f85e1c51015b748e46607771
Hocine <hocine.hammouche@hotmail.com>
2021-06-10 12:33:10 +0200

    Added an object to represent the board and the players

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
 	println!("Hello, world!");
 }
+
+enum Player { Cross, Circle, None }
+
+struct Board([[Player; 3]; 3]);
+
+impl Board {
+	fn new() -> Board {
+		Board([[Player::None; 3]; 3])
+	}
+}
